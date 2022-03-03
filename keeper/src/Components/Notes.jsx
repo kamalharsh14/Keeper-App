848f2402ef11b5f9342d4bb6220cb777a6b123dd
Harsh Kamal Singh <singhharsh998@gmail.com>
2022-03-03 10:44:56 +0530

    Initial Commit

--- keeper/src/Components/Notes.jsx
+++ keeper/src/Components/Notes.jsx
@@ -2,22 +2,12 @@ import React from "react";
 import '../App.css';
 
 function Notes(){
-
-    const date = new Date();
-    const day = date.getDay()
-    const month = date.getMonth()
-    const year = date.getFullYear()
     return (
-        <div class="card">
-        <div class="card-header">
-        Title
-        </div>
-        <div class="card-body">
-        <blockquote class="blockquote mb-0">
-            <p>A well-known quote, contained in a blockquote element.</p>
-            <footer class="blockquote-footer">{day}/{month}/{year}</footer>
-        </blockquote>
-        </div>
+        <div className="card">
+            <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
         </div>
     );
 }
